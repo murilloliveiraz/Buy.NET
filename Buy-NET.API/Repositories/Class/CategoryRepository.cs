4be8a07263a7e7738adf5b612b3ba6fcc012cbe9
Murillo Oliveira <murillo1047@gmail.com>
2024-07-05 11:16:28 -0300

    feat: category repository

---
+++ Buy-NET.API/Repositories/Class/CategoryRepository.cs
@@ -0,0 +1,48 @@
+using Buy_NET.API.Data.Contexts;
+using Buy_NET.API.Domain.Models;
+using Buy_NET.API.Repositories.Interfaces.CategoryRepositoryInterface;
+using Microsoft.EntityFrameworkCore;
+
+namespace Buy_NET.API.Repositories.Class;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private ApplicationContext _context;
+
+    public CategoryRepository(ApplicationContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Category> Create(Category model)
+    {
+        await _context.Category.AddAsync(model);
+        await _context.SaveChangesAsync();
+
+        return model;
+    }
+
+    public async Task Delete(Category model)
+    {
+        model.InactivationDate = DateTime.Now;
+        await Update(model);
+    }
+
+    public async Task<IEnumerable<Category?>> Get()
+    {
+        return await _context.Category.AsNoTracking().OrderBy(c => c.Id).ToListAsync();
+    }
+
+    public async Task<Category?> GetById(long id)
+    {
+        return await _context.Category.AsNoTracking().Where(c => c.Id == id).FirstOrDefaultAsync();
+    }
+
+    public async Task<Category> Update(Category model)
+    {
+        Category? entityAtDatabase = await _context.Category.Where(c => c.Id == model.Id).FirstOrDefaultAsync();
+        _context.Entry(entityAtDatabase).CurrentValues.SetValues(model);
+        await _context.SaveChangesAsync();
+        return entityAtDatabase;
+    }
+}
---
+++ Buy-NET.API/Repositories/Interfaces/CategoryRepositoryInterface/ICategoryRepository.cs
@@ -0,0 +1,5 @@
+using Buy_NET.API.Domain.Models;
+
+namespace Buy_NET.API.Repositories.Interfaces.CategoryRepositoryInterface;
+
+public interface ICategoryRepository : IRepository<Category, long>{}
